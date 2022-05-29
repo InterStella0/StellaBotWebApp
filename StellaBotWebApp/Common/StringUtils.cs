9f61c07268b7031f5c2729c87c045e60cb2e8bbb
Stella <sarahprog667@gmail.com>
2022-05-30 02:28:29 +0800

    extra handling on no api

---
+++ StellaBotWebApp/Common/StringUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace StellaBotWebApp.Common
+{
+    public static class StringUtils
+    {
+        public static string ShowValue(string? value)
+        {
+            return value is null || value.Equals("") ? "N/A": value;
+        }
+    }
+}
--- StellaBotWebApp/Models/StellaAPI.cs
+++ StellaBotWebApp/Models/StellaAPI.cs
@@ -4,19 +4,18 @@ using Newtonsoft.Json.Linq;
 
 namespace StellaBotWebApp.Models
 {
-
     public class APIBotInfo
     {
-        public string Avatar { get; set; }
-        public string OwnerAvatar { get; set; }
-        public string OwnerName { get; set; }
-        public string Name { get; set; }
-        public long GuildAmount { get; set; }
-        public long UserAmount { get; set; }
+        public string? Avatar { get; set; }
+        public string? OwnerAvatar { get; set; }
+        public string? OwnerName { get; set; }
+        public string? Name { get; set; }
+        public long? GuildAmount { get; set; }
+        public long? UserAmount { get; set; }
         public List<Dictionary<string, object>> LastCommands { get; set; }
-        public double Latency { get; set; }
-        public long CodeLines { get; set; }
-        public DateTime UpTime { get; set; }
+        public double? Latency { get; set; }
+        public long? CodeLines { get; set; }
+        public DateTime? UpTime { get; set; }
     }
     public class StellaAPI
     {
@@ -39,30 +38,52 @@ namespace StellaBotWebApp.Models
             var jsonString = await response.Content.ReadAsStringAsync();
             return strToJson<T>(jsonString);
         }
-        public async Task<Dictionary<string, string>> GetRequestJsonAsync(string path)
+        public async Task<Dictionary<string, string>?> GetRequestJsonAsync(string path)
         {
-            return await GetRequestAsync<Dictionary<string, string>>(path);
+            try
+            {
+                return await GetRequestAsync<Dictionary<string, string>>(path);
+            }catch (HttpRequestException ex)
+            {
+                return null;
+            }
+        }
+        private T? GetValueDict<T>(Dictionary<string, T> dict, string key)
+        {
+            return dict is not null ? dict[key] : default;
+        }
+        private T? GetValueDictObj<T>(Dictionary<string, object> dict, string key)
+        {
+            return dict is not null ? (T) dict[key] : default;
         }
         public async Task Init()
         {
             var botResult = await GetRequestJsonAsync("name");
             var ownerResult = await GetRequestJsonAsync("owner_name");
-            var botRealTimeInfo = await GetRequestAsync<Dictionary<string, object>>("info");
-
-            var lastCommands = ((JArray) botRealTimeInfo["last_commands"]).ToObject<List<Dictionary<string, object>>>();
             BotInfo = new APIBotInfo()
             {
                 Avatar = FormUrl("stella_bot", "avatar"),
                 OwnerAvatar = FormUrl("stella_bot", "owner_avatar"),
-                Name = botResult["full"],
-                OwnerName = ownerResult["full"],
-                GuildAmount = (long) botRealTimeInfo["guild_amount"],
-                UserAmount = (long) botRealTimeInfo["user_amount"],
-                LastCommands = lastCommands,
-                Latency = (double) botRealTimeInfo["latency"],
-                CodeLines = (long) botRealTimeInfo["codelines"],
-                UpTime = (DateTime) botRealTimeInfo["launch_time"],
+                Name = GetValueDict(botResult, "full"),
+                OwnerName = GetValueDict(ownerResult, "full")
             };
+            try
+            {
+                var botRealTimeInfo = await GetRequestAsync<Dictionary<string, object>>("info");
+                var lastCommands = ((JArray)botRealTimeInfo["last_commands"]).ToObject<List<Dictionary<string, object>>>();
+
+                BotInfo.GuildAmount = GetValueDictObj<long>(botRealTimeInfo, "guild_amount");
+                BotInfo.UserAmount = GetValueDictObj<long>(botRealTimeInfo, "user_amount");
+                BotInfo.LastCommands = lastCommands;
+                BotInfo.Latency = GetValueDictObj<double>(botRealTimeInfo, "latency");
+                BotInfo.CodeLines = GetValueDictObj<long>(botRealTimeInfo, "codelines");
+                BotInfo.UpTime = GetValueDictObj<DateTime>(botRealTimeInfo, "launch_time");
+            }
+            catch (HttpRequestException ex)
+            {
+                BotInfo.LastCommands = new List<Dictionary<string, object>>();
+            }
+
         }
         public static string ToHumanReadableString(TimeSpan t)
         {
